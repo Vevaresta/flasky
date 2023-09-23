68560e1f38104fe6583f7de729361c0fab277efe
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2023-09-23 21:03:15 +0200

    added dynamic route

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
-print("Hello World")
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# this decorator registers function index() 
+# as the handler for the app root URL
+@app.route("/")
+def index():
+    return "<h1>Hello World!</h1>"
+
+# Dynamic route
+@app.route("/user/<name>")
+def user(name):
+    return f"<h1>Hello, {name}!</h1>"
