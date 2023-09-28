4f8969457ee57541733e2f6345c1d414d2492593
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2023-09-28 10:58:28 +0200

    Migration db

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, flash
+from flask import Flask, render_template, session, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -6,6 +6,8 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
@@ -20,6 +22,7 @@ app.config["SECRET_KEY"] = "hard to guess string"
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+migrate = Migrate(app, db)
 
 
 class NameForm(FlaskForm):
@@ -42,17 +45,22 @@ def internal_server_error(e):
     return render_template("500.html"), 500
 
 
-@app.route("/", methods=["GET", "POST"])
-# endpoint name = index
+@app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name!")
-        session["name"] = form.name.data
-        return redirect(url_for("index"))
-    return render_template("index.html", form=form, name=session.get("name"))
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 class Role(db.Model):
@@ -73,3 +81,8 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username  
+    
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
